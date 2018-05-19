40e454a709a860dbb5d95cac430bd66c8dde15d3
Alex Lian <alian@unity3d.com>
2018-05-19 08:30:49 +0200

    Force test projects to have  the correct project settings

--- build.py
+++ build.py
@@ -8,6 +8,23 @@ def packages_list():
         ("com.unity.mathematics", os.path.join("src"))
     ]
 
+# Prepare an empty project for editor tests
+def prepare_editor_test_project(repo_path, project_path, logger):
+    import unity_package_build
+    settings_to_swap = {
+        "scriptingRuntimeVersion: 0": "scriptingRuntimeVersion: 1",
+        "apiCompatibilityLevel: 2": "apiCompatibilityLevel: 3"
+    }
+    projectsettings_path = os.path.join(project_path, "ProjectSettings", "ProjectSettings.asset")
+    settings = ""
+    with open(projectsettings_path) as f:
+        settings = f.read()
+        for key, value in settings_to_swap.iteritems():
+            settings = settings.replace(key, value)
+
+    with open(projectsettings_path, 'w') as f:
+        f.write(settings)
+
 if __name__ == "__main__":
     import sys
     sys.path.insert(0, os.path.abspath(os.path.join("..", "automation-tools")))
