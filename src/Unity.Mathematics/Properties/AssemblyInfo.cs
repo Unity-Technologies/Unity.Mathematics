48b44c0746dc8c855644d8dc64af82f1fea3e006
Lucas Meijer <lucas@lucasmeijer.com>
2019-06-17 09:15:19 +0200

    ifdef out references to unityengine & unsupported attributes for dots runtime (#70)
    * ifdef out access to unityengine types
    
    * Dots runtime compatibility

--- src/Unity.Mathematics/Properties/AssemblyInfo.cs
+++ src/Unity.Mathematics/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+#if !UNITY_DOTSPLAYER
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -38,3 +39,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+#endif
