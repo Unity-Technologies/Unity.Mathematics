1f400d4205d5cadbfb298594d75cb6dd41402836
Dale Kim <dale@unity3d.com>
2019-12-03 18:34:47 -0800

    Added some more tests for conversions.
    Should just code generate these tests.

--- src/Unity.Mathematics.PerformanceTests/MonoBehaviour1.cs
+++ src/Unity.Mathematics.PerformanceTests/MonoBehaviour1.cs
@@ -59,11 +59,28 @@ namespace Unity.Mathematics.PerformanceTests
                 .MeasurementCount(10)
                 .Run();
         }
+
+        [Test, Performance]
+        public void Float4x4_Float4x4()
+        {
+            var m1 = float4x4.identity;
+            var m2 = float4x4.identity;
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        m1 = math.mul(m1, m2);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
     }
 
     public class Conversions
     {
-
         [Test, Performance]
         public void QuaternionToFloat3x3()
         {
@@ -75,6 +92,79 @@ namespace Unity.Mathematics.PerformanceTests
                     for (int i = 0; i < 10000; ++i)
                     {
                         f3x3 = new float3x3(q1);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void QuaternionToRigidTransform()
+        {
+            var q1 = quaternion.identity;
+            var r = RigidTransform.identity;
+            var position = new float3();
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        r = new RigidTransform(q1, position);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void Float3x3ToQuaternion()
+        {
+            var q1 = new quaternion();
+            var f3x3 = float3x3.identity;
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        q1 = new quaternion(f3x3);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void Float4x4ToQuaternion()
+        {
+            var q1 = new quaternion();
+            var f4x4 = float4x4.identity;
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        q1 = new quaternion(f4x4);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void Float4ToUint4()
+        {
+            var f4 = new float4(1.0f, 2.0f, 3.0f, 4.0f);
+            var u4 = new uint4();
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        u4 = math.asuint(f4);
                     }
                 })
                 .WarmupCount(1)
