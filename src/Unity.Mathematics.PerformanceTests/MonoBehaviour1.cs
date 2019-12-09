77d4eec97b8605782e299b3345e74ad4ffe2d163
Dale Kim <dale@unity3d.com>
2019-12-09 14:55:40 -0800

    Added some sincos perf tests.

--- src/Unity.Mathematics.PerformanceTests/MonoBehaviour1.cs
+++ src/Unity.Mathematics.PerformanceTests/MonoBehaviour1.cs
@@ -376,4 +376,83 @@ namespace Unity.Mathematics.PerformanceTests
                 .Run();
         }
     }
+
+    public class TestTrig
+    {
+        [Test, Performance]
+        public void sincos4()
+        {
+            var f4 = new float4(1.0f, 2.0f, 3.0f, 4.0f);
+            var sin = new float4();
+            var cos = new float4();
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        math.sincos(f4, out sin, out cos);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void sincos3()
+        {
+            var f3 = new float3(1.0f, 2.0f, 3.0f);
+            var sin = new float3();
+            var cos = new float3();
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        math.sincos(f3, out sin, out cos);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void sincos2()
+        {
+            var f2 = new float2(1.0f, 2.0f);
+            var sin = new float2();
+            var cos = new float2();
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        math.sincos(f2, out sin, out cos);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+
+        [Test, Performance]
+        public void sincos()
+        {
+            var f = 1.0f;
+            var sin = 0.0f;
+            var cos = 0.0f;
+
+            Measure.Method(() =>
+                {
+                    for (int i = 0; i < 10000; ++i)
+                    {
+                        math.sincos(f, out sin, out cos);
+                    }
+                })
+                .WarmupCount(1)
+                .MeasurementCount(10)
+                .Run();
+        }
+    }
 }
