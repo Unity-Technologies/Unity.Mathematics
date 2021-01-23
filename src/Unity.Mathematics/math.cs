935e2970504d3931b21b4cecd867dd56a3f9a6ea
Dale Kim <3121968+unpacklo@users.noreply.github.com>
2021-01-22 16:37:27 -0800

    sincos docs.
    1873.

--- src/Unity.Mathematics/math.cs
+++ src/Unity.Mathematics/math.cs
@@ -3162,35 +3162,67 @@ namespace Unity.Mathematics
 
 
         /// <summary>Returns the sine and cosine of the input float value x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input angle in radians.</param>
+        /// <param name="s">Output sine of the input.</param>
+        /// <param name="c">Output cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(float x, out float s, out float c) { s = sin(x); c = cos(x); }
 
         /// <summary>Returns the componentwise sine and cosine of the input float2 vector x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input vector containing angles in radians.</param>
+        /// <param name="s">Output vector containing the componentwise sine of the input.</param>
+        /// <param name="c">Output vector containing the componentwise cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(float2 x, out float2 s, out float2 c) { s = sin(x); c = cos(x); }
 
         /// <summary>Returns the componentwise sine and cosine of the input float3 vector x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input vector containing angles in radians.</param>
+        /// <param name="s">Output vector containing the componentwise sine of the input.</param>
+        /// <param name="c">Output vector containing the componentwise cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(float3 x, out float3 s, out float3 c) { s = sin(x); c = cos(x); }
 
         /// <summary>Returns the componentwise sine and cosine of the input float4 vector x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input vector containing angles in radians.</param>
+        /// <param name="s">Output vector containing the componentwise sine of the input.</param>
+        /// <param name="c">Output vector containing the componentwise cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(float4 x, out float4 s, out float4 c) { s = sin(x); c = cos(x); }
 
 
         /// <summary>Returns the sine and cosine of the input double value x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input angle in radians.</param>
+        /// <param name="s">Output sine of the input.</param>
+        /// <param name="c">Output cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(double x, out double s, out double c) { s = sin(x); c = cos(x); }
 
         /// <summary>Returns the componentwise sine and cosine of the input double2 vector x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input vector containing angles in radians.</param>
+        /// <param name="s">Output vector containing the componentwise sine of the input.</param>
+        /// <param name="c">Output vector containing the componentwise cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(double2 x, out double2 s, out double2 c) { s = sin(x); c = cos(x); }
 
         /// <summary>Returns the componentwise sine and cosine of the input double3 vector x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input vector containing angles in radians.</param>
+        /// <param name="s">Output vector containing the componentwise sine of the input.</param>
+        /// <param name="c">Output vector containing the componentwise cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(double3 x, out double3 s, out double3 c) { s = sin(x); c = cos(x); }
 
         /// <summary>Returns the componentwise sine and cosine of the input double4 vector x through the out parameters s and c.</summary>
+        /// <remarks>When Burst compiled, his method is faster than calling sin() and cos() separately.</remarks>
+        /// <param name="x">Input vector containing angles in radians.</param>
+        /// <param name="s">Output vector containing the componentwise sine of the input.</param>
+        /// <param name="c">Output vector containing the componentwise cosine of the input.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void sincos(double4 x, out double4 s, out double4 c) { s = sin(x); c = cos(x); }
 
