7dbc83edb8df58c99134b3dc709cac917c110924
Dale Kim <3121968+unpacklo@users.noreply.github.com>
2021-11-12 14:44:02 -0800

    Move quat to euler decomposition to math class.
    Previously wanted it in float3 but that's problematic because Burst
    can fail to compile a vector type if it contains methods it doesn't
    expect.

--- src/Tests/Tests/Shared/TestMath.cs
+++ src/Tests/Tests/Shared/TestMath.cs
@@ -3881,5 +3881,78 @@ namespace Unity.Mathematics.Tests
             float4 c = chgsign(float4(1f, 2f, 3f, -4f), float4(-1f, 0f, -0f, -23.56f));
             TestUtils.IsTrue(all(c == float4(-1f, 2f, -3f, 4f)));
         }
+
+        [TestCompiler]
+        public static unsafe void quaternion_to_euler()
+        {
+            const float epsilon = 0.0045f;
+            const float PI_div_6 = PI / 6f;
+            const int testAngleCount = 13 * 13 * 13 * 20;
+
+            float3* testAngles = stackalloc float3[testAngleCount];
+
+            int idx = 0;
+            for (int i = 0; i < 13; ++i)
+            {
+                float x = -PI + i * PI;
+                for (int j = 0; j < 13; ++j)
+                {
+                    float y = -PI + j * PI_div_6;
+                    for (int k = 0; k < 13; ++k)
+                    {
+                        float z = -PI + k * PI_div_6;
+                        for (int l = 0; l < 20; ++l)
+                        {
+                            testAngles[idx++] = float3(x, y, z) * (0.99f + l * 0.001f);
+                        }
+                    }
+                }
+            }
+
+            for (int order = 0; order < 6; ++order)
+            {
+                for (int i = 0; i < testAngleCount; ++i)
+                {
+                    // We cannot directly compare Euler angles, as different angles may represent the same rotation,
+                    // so we have to transform them back to quats to test.
+                    // We also need to create our initial test quaternion, as what we generated was euler angles.
+                    quaternion expected = TestRefEulerToQuat(testAngles[i], math.RotationOrder.Default);
+
+                    float3 actualEulers = Euler(expected, (math.RotationOrder)order);
+                    quaternion actualQuat = TestRefEulerToQuat(actualEulers, (math.RotationOrder)order);
+
+                    float error = angle(expected, actualQuat);
+                    TestUtils.AreEqual(0f, error, epsilon);
+                }
+            }
+        }
+
+        static quaternion TestRefEulerToQuat(float3 angle, math.RotationOrder order)
+        {
+            float3 halfRot = angle * 0.5f;
+            sincos(halfRot, out float3 s, out float3 c);
+
+            quaternion qX = quaternion(s.x, 0f, 0f, c.x);
+            quaternion qY = quaternion(0f, s.y, 0f, c.y);
+            quaternion qZ = quaternion(0f, 0f, s.z, c.z);
+
+            switch (order)
+            {
+                case math.RotationOrder.XYZ:
+                    return mul(mul(qZ, qY), qX);
+                case math.RotationOrder.XZY:
+                    return mul(mul(qY, qZ), qX);
+                case math.RotationOrder.YXZ:
+                    return mul(mul(qZ, qX), qY);
+                case math.RotationOrder.YZX:
+                    return mul(mul(qX, qZ), qY);
+                case math.RotationOrder.ZXY:
+                    return mul(mul(qY, qX), qZ);
+                case math.RotationOrder.ZYX:
+                    return mul(mul(qX, qY), qZ);
+                default:
+                    return quaternion(float4(1f));
+            }
+        }
     }
 }
--- src/Unity.Mathematics/math.cs
+++ src/Unity.Mathematics/math.cs
@@ -6617,6 +6617,276 @@ namespace Unity.Mathematics
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float3 right() { return new float3(1.0f, 0.0f, 0.0f); }
 
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion following the XYZ rotation order.
+        /// All rotation angles are in radians and clockwise when looking along the rotation axis towards the origin.
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <returns>The Euler angle representation of the quaternion in XYZ order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 EulerXYZ(quaternion q)
+        {
+            const float epsilon = 1e-6f;
+            const float cutoff = (1f - 2f * epsilon) * (1f - 2f * epsilon);
+
+            // prepare the data
+            var qv = q.value;
+            var d1 = qv * qv.wwww * float4(2f); //xw, yw, zw, ww
+            var d2 = qv * qv.yzxw * float4(2f); //xy, yz, zx, ww
+            var d3 = qv * qv;
+            var euler = Unity.Mathematics.float3.zero;
+
+            var y1 = d2.z - d1.y;
+            if (y1 * y1 < cutoff)
+            {
+                var x1 = d2.y + d1.x;
+                var x2 = d3.z + d3.w - d3.y - d3.x;
+                var z1 = d2.x + d1.z;
+                var z2 = d3.x + d3.w - d3.y - d3.z;
+                euler = float3(atan2(x1, x2), -asin(y1), atan2(z1, z2));
+            }
+            else //xzx
+            {
+                y1 = clamp(y1, -1f, 1f);
+                var abcd = float4(d2.z, d1.y, d2.x, d1.z);
+                var x1 = 2f * (abcd.x * abcd.w + abcd.y * abcd.z); //2(ad+bc)
+                var x2 = csum(abcd * abcd * float4(-1f, 1f, -1f, 1f));
+                euler = float3(atan2(x1, x2), -asin(y1), 0f);
+            }
+
+            return euler;
+        }
+
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion following the XZY rotation order.
+        /// All rotation angles are in radians and clockwise when looking along the rotation axis towards the origin.
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <returns>The Euler angle representation of the quaternion in XZY order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 EulerXZY(quaternion q)
+        {
+            const float epsilon = 1e-6f;
+            const float cutoff = (1f - 2f * epsilon) * (1f - 2f * epsilon);
+
+            // prepare the data
+            var qv = q.value;
+            var d1 = qv * qv.wwww * float4(2f); //xw, yw, zw, ww
+            var d2 = qv * qv.yzxw * float4(2f); //xy, yz, zx, ww
+            var d3 = qv * qv;
+            var euler = Unity.Mathematics.float3.zero;
+
+            var y1 = d2.x + d1.z;
+            if (y1 * y1 < cutoff)
+            {
+                var x1 = -d2.y + d1.x;
+                var x2 = d3.y + d3.w - d3.z - d3.x;
+                var z1 = -d2.z + d1.y;
+                var z2 = d3.x + d3.w - d3.y - d3.z;
+                euler = float3(atan2(x1, x2), asin(y1), atan2(z1, z2));
+            }
+            else //xyx
+            {
+                y1 = clamp(y1, -1f, 1f);
+                var abcd = float4(d2.x, d1.z, d2.z, d1.y);
+                var x1 = 2f * (abcd.x * abcd.w + abcd.y * abcd.z); //2(ad+bc)
+                var x2 = csum(abcd * abcd * float4(-1f, 1f, -1f, 1f));
+                euler = float3(atan2(x1, x2), asin(y1), 0f);
+            }
+
+            return euler.xzy;
+        }
+
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion following the YXZ rotation order.
+        /// All rotation angles are in radians and clockwise when looking along the rotation axis towards the origin.
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <returns>The Euler angle representation of the quaternion in YXZ order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 EulerYXZ(quaternion q)
+        {
+            const float epsilon = 1e-6f;
+            const float cutoff = (1f - 2f * epsilon) * (1f - 2f * epsilon);
+
+            // prepare the data
+            var qv = q.value;
+            var d1 = qv * qv.wwww * float4(2f); //xw, yw, zw, ww
+            var d2 = qv * qv.yzxw * float4(2f); //xy, yz, zx, ww
+            var d3 = qv * qv;
+            var euler = Unity.Mathematics.float3.zero;
+
+            var y1 = d2.y + d1.x;
+            if (y1 * y1 < cutoff)
+            {
+                var x1 = -d2.z + d1.y;
+                var x2 = d3.z + d3.w - d3.x - d3.y;
+                var z1 = -d2.x + d1.z;
+                var z2 = d3.y + d3.w - d3.z - d3.x;
+                euler = float3(atan2(x1, x2), asin(y1), atan2(z1, z2));
+            }
+            else //yzy
+            {
+                y1 = clamp(y1, -1f, 1f);
+                var abcd = float4(d2.x, d1.z, d2.y, d1.x);
+                var x1 = 2f * (abcd.x * abcd.w + abcd.y * abcd.z); //2(ad+bc)
+                var x2 = csum(abcd * abcd * float4(-1f, 1f, -1f, 1f));
+                euler = float3(atan2(x1, x2), asin(y1), 0f);
+            }
+
+            return euler.yxz;
+        }
+
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion following the YZX rotation order.
+        /// All rotation angles are in radians and clockwise when looking along the rotation axis towards the origin.
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <returns>The Euler angle representation of the quaternion in YZX order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 EulerYZX(quaternion q)
+        {
+            const float epsilon = 1e-6f;
+            const float cutoff = (1f - 2f * epsilon) * (1f - 2f * epsilon);
+
+            // prepare the data
+            var qv = q.value;
+            var d1 = qv * qv.wwww * float4(2f); //xw, yw, zw, ww
+            var d2 = qv * qv.yzxw * float4(2f); //xy, yz, zx, ww
+            var d3 = qv * qv;
+            var euler = Unity.Mathematics.float3.zero;
+
+            var y1 = d2.x - d1.z;
+            if (y1 * y1 < cutoff)
+            {
+                var x1 = d2.z + d1.y;
+                var x2 = d3.x + d3.w - d3.z - d3.y;
+                var z1 = d2.y + d1.x;
+                var z2 = d3.y + d3.w - d3.x - d3.z;
+                euler = float3(atan2(x1, x2), -asin(y1), atan2(z1, z2));
+            }
+            else //yxy
+            {
+                y1 = clamp(y1, -1f, 1f);
+                var abcd = float4(d2.x, d1.z, d2.y, d1.x);
+                var x1 = 2f * (abcd.x * abcd.w + abcd.y * abcd.z); //2(ad+bc)
+                var x2 = csum(abcd * abcd * float4(-1f, 1f, -1f, 1f));
+                euler = float3(atan2(x1, x2), -asin(y1), 0f);
+            }
+
+            return euler.zxy;
+        }
+
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion following the ZXY rotation order.
+        /// All rotation angles are in radians and clockwise when looking along the rotation axis towards the origin.
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <returns>The Euler angle representation of the quaternion in ZXY order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 EulerZXY(quaternion q)
+        {
+            const float epsilon = 1e-6f;
+            const float cutoff = (1f - 2f * epsilon) * (1f - 2f * epsilon);
+
+            // prepare the data
+            var qv = q.value;
+            var d1 = qv * qv.wwww * float4(2f); //xw, yw, zw, ww
+            var d2 = qv * qv.yzxw * float4(2f); //xy, yz, zx, ww
+            var d3 = qv * qv;
+            var euler = Unity.Mathematics.float3.zero;
+
+            var y1 = d2.y - d1.x;
+            if (y1 * y1 < cutoff)
+            {
+                var x1 = d2.x + d1.z;
+                var x2 = d3.y + d3.w - d3.x - d3.z;
+                var z1 = d2.z + d1.y;
+                var z2 = d3.z + d3.w - d3.x - d3.y;
+                euler = float3(atan2(x1, x2), -asin(y1), atan2(z1, z2));
+            }
+            else //zxz
+            {
+                y1 = clamp(y1, -1f, 1f);
+                var abcd = float4(d2.z, d1.y, d2.y, d1.x);
+                var x1 = 2f * (abcd.x * abcd.w + abcd.y * abcd.z); //2(ad+bc)
+                var x2 = csum(abcd * abcd * float4(-1f, 1f, -1f, 1f));
+                euler = float3(atan2(x1, x2), -asin(y1), 0f);
+            }
+
+            return euler.yzx;
+        }
+
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion following the ZYX rotation order.
+        /// All rotation angles are in radians and clockwise when looking along the rotation axis towards the origin.
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <returns>The Euler angle representation of the quaternion in ZYX order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 EulerZYX(quaternion q)
+        {
+            const float epsilon = 1e-6f;
+            const float cutoff = (1f - 2f * epsilon) * (1f - 2f * epsilon);
+
+            var qv = q.value;
+            var d1 = qv * qv.wwww * float4(2f); //xw, yw, zw, ww
+            var d2 = qv * qv.yzxw * float4(2f); //xy, yz, zx, ww
+            var d3 = qv * qv;
+            var euler = Unity.Mathematics.float3.zero;
+
+            var y1 = d2.z + d1.y;
+            if (y1 * y1 < cutoff)
+            {
+                var x1 = -d2.x + d1.z;
+                var x2 = d3.x + d3.w - d3.y - d3.z;
+                var z1 = -d2.y + d1.x;
+                var z2 = d3.z + d3.w - d3.y - d3.x;
+                euler = float3(atan2(x1, x2), asin(y1), atan2(z1, z2));
+            }
+            else //zxz
+            {
+                y1 = clamp(y1, -1f, 1f);
+                var abcd = float4(d2.z, d1.y, d2.y, d1.x);
+                var x1 = 2f * (abcd.x * abcd.w + abcd.y * abcd.z); //2(ad+bc)
+                var x2 = csum(abcd * abcd * float4(-1f, 1f, -1f, 1f));
+                euler = float3(atan2(x1, x2), asin(y1), 0f);
+            }
+
+            return euler.zyx;
+        }
+
+        /// <summary>
+        /// Returns the Euler angle representation of the quaternion. Angles returned depend on the specified order to apply the
+        /// 3 rotations around the principal axes. All rotation angles are in radians and clockwise when looking along the
+        /// rotation axis towards the origin.
+        /// When the rotation order is known at compile time, it is recommended for performance reasons to use specific
+        /// Euler rotation constructors such as EulerZXY(...).
+        /// </summary>
+        /// <param name="q">The quaternion to convert to Euler angles.</param>
+        /// <param name="order">The order in which the rotations are applied.</param>
+        /// <returns>The Euler angle representation of the quaternion in the specified order.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 Euler(quaternion q, math.RotationOrder order = math.RotationOrder.Default)
+        {
+            switch (order)
+            {
+                case math.RotationOrder.XYZ:
+                    return EulerXYZ(q);
+                case math.RotationOrder.XZY:
+                    return EulerXZY(q);
+                case math.RotationOrder.YXZ:
+                    return EulerYXZ(q);
+                case math.RotationOrder.YZX:
+                    return EulerYZX(q);
+                case math.RotationOrder.ZXY:
+                    return EulerZXY(q);
+                case math.RotationOrder.ZYX:
+                    return EulerZYX(q);
+                default:
+                    return Unity.Mathematics.float3.zero;
+            }
+        }
 
         // Internal
 
