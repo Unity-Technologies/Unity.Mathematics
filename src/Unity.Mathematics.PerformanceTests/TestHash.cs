c721867cd263e4ade4d939ade960bdcabe0e15db
Dale Kim <3121968+unpacklo@users.noreply.github.com>
2023-08-14 19:44:46 -0700

    Clean up performance test.

--- src/Unity.Mathematics.PerformanceTests/TestHash.cs
+++ src/Unity.Mathematics.PerformanceTests/TestHash.cs
@@ -11,37 +11,46 @@ namespace Unity.Mathematics.PerformanceTests
     partial class TestHash
     {
         static readonly long kBufferBytes = 1024 * 1024;
+        static readonly long kBytesToHash = kBufferBytes - 1;
+        static readonly uint kExpectedHash = 1781740882;
 
         [Test, Performance]
         public static unsafe void HashAligned()
         {
-            Debug.Log(Hash(false));
+            Assert.AreEqual(kExpectedHash, Hash(false));
         }
 
         [Test, Performance]
         public static unsafe void HashUnaligned()
         {
-            Debug.Log(Hash(true));
+            Assert.AreEqual(kExpectedHash, Hash(true));
         }
 
         [BurstCompile(CompileSynchronously = true)]
         static unsafe uint Hash(bool useUnalignedBuffer)
         {
-            var ulongptr = (ulong)(UIntPtr)UnsafeUtility.Malloc(kBufferBytes, 16, Collections.Allocator.Persistent);
-            var unaligned = ulongptr;
+            var ptr = (ulong)(UIntPtr)UnsafeUtility.Malloc(kBufferBytes, 16, Collections.Allocator.Persistent);
+            var ulongptr = (ulong)ptr;
             uint seed = 0;
 
-            if (useUnalignedBuffer && ((unaligned & 1) == 0))
+            if (useUnalignedBuffer && ((ulongptr & 1) == 0))
             {
-                ++unaligned;
+                ++ulongptr;
+            }
+
+            var buffer = (void*)ulongptr;
+
+            for (int i = 0; i < kBytesToHash; ++i)
+            {
+                ((byte*)buffer)[i] = 123;
             }
 
             for (int i = 0; i < 100; ++i)
             {
-                seed = math.hash((void*)unaligned, (int)kBufferBytes - 1, seed);
+                seed = math.hash(buffer, (int)kBytesToHash, seed);
             }
 
-            UnsafeUtility.Free((void*)ulongptr, Collections.Allocator.Persistent);
+            UnsafeUtility.Free((void*)ptr, Collections.Allocator.Persistent);
 
             return seed;
         }
