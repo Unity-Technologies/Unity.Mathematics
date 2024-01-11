5fe7e8653c7285265eb3dfe5bee551b2a8c46b2e
Scott Williams <scott.williams@unity3d.com>
2024-01-10 16:46:19 -0800

    Update package verification package and re-add internals visible to for testing

---
+++ src/Unity.Mathematics/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("Unity.Mathematics.Tests")]
+[assembly: InternalsVisibleTo("Unity.Mathematics.PerformanceTests")]
